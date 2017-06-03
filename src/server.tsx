10bbd480702fd67b6197696ef780493e79ba575b
Andrew Bryson <modernenglishcoaching@gmail.com>
2017-06-03 22:56:33 +0200

    added compression middleware

--- src/server.tsx
+++ src/server.tsx
@@ -2,6 +2,7 @@ import * as http from 'http';
 import * as express from 'express';
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
+const compression = require('compression');
 import { Router, RouterContext, match } from 'react-router';
 import { getAllComponentsCSS } from './utils/css_styler';
 import { store } from "./redux/store";
@@ -11,6 +12,9 @@ import routes from './routes';
 const release = (process.env.NODE_ENV === 'production');
 const port = (parseInt(process.env.PORT, 10) || 3000) - (!release as any);
 const app = express();
+
+//compress requests
+app.use(compression());
 
 // Set view engine
 app.set('views', './src/server/views');
